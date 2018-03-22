fb457ee9cd87a0f1a2df29e328c52ce1eb221337
Daniel Meltzer <meltzerdanielebay@gmail.com>
2018-03-22 10:40:35 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CS_TAU_C_Plus_Plus_First_Exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_TAU_C_Plus_Plus_First_Exercise.dir/main.cpp.o"
+  "CS_TAU_C_Plus_Plus_First_Exercise.pdb"
+  "CS_TAU_C_Plus_Plus_First_Exercise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS_TAU_C_Plus_Plus_First_Exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CS_TAU_C_Plus_Plus_First_Exercise.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS_TAU_C_Plus_Plus_First_Exercise.
+# This may be replaced when dependencies are built.
