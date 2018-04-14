9c81a1f867c2aa758f2f3206b7b2ce13a7283417
itamar <itamarcu@gmail.com>
2018-04-14 13:16:12 +0300

    typo fix (splitedStrings)

--- BoardIO.cpp
+++ BoardIO.cpp
@@ -9,22 +9,22 @@ Board *BoardIO::parse_file(std::string& path) {
 }
 
 std::vector<std::string> *BoardIO::split_string_using_delimiter(std::string& stringToSplit, std::string& delimiter) {
-        auto splitedStrings = new std::vector<std::string>;
+        auto splittedStrings = new std::vector<std::string>;
         size_t pos = stringToSplit.find(delimiter);
         size_t initialPos = 0;
         stringToSplit.clear();
 
 
         while( pos != std::string::npos ) {
-            (*splitedStrings).push_back( stringToSplit.substr( initialPos, pos - initialPos ) );
+            (*splittedStrings).push_back( stringToSplit.substr( initialPos, pos - initialPos ) );
             initialPos = pos + delimiter.length();
 
             pos = stringToSplit.find( delimiter, initialPos );
         }
 
         // Add the last one
-        (*splitedStrings).push_back(stringToSplit.substr( initialPos, std::min( pos, stringToSplit.size() ) - initialPos + 1 ) );
+        (*splittedStrings).push_back(stringToSplit.substr( initialPos, std::min( pos, stringToSplit.size() ) - initialPos + 1 ) );
 
-        return splitedStrings;
+        return splittedStrings;
 
 }
