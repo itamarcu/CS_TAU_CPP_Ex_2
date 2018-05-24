e9df5c577c8b20d945d7f780990539b93895de9c
Daniel Meltzer <meltzerdanielebay@gmail.com>
2018-05-24 14:23:38 +0300

    adding more docs

--- MyFightInfo.h
+++ MyFightInfo.h
@@ -18,9 +18,19 @@ class MyFightInfo : public FightInfo {
 public:
     MyFightInfo(int winner, const MyPoint &position, char firstPlayerPiece, char secondPlayerPiece);
 
+    /**
+     * get position of fight
+     * @return fight position
+     */
     const Point &getPosition() const override;
 
+    /**
+     * get piece
+     * @param player for player 1 this should be FIRST_PLAYER_CONST, for player 2 this should be SECOND_PLAYER_CONST
+     * @return piece char
+     */
     char getPiece(int player) const override;
+
     /**
      * get fight winner
      * @return 0 - if both lose / tie, FIRST_PLAYER_CONST - if first player wins and SECOND_PLAYER_CONST - if
