3f6cc9a131b80a83e2f32c74c19522b73cfd3a2b
Itamar Curiel <itamarcu@gmail.com>
2018-05-25 14:10:30 +0300

    fixed typo mistake (char from type, type from char)

--- GamePiece.cpp
+++ GamePiece.cpp
@@ -77,7 +77,7 @@ GamePieceType type_from_char(char c) {
     }
 }
 
-char nonjoker_lowercase_type_from_char(GamePieceType gamePieceType) {
+char nonjoker_lowercase_char_from_type(GamePieceType gamePieceType) {
     char ch;
     switch (gamePieceType) {
         case Rock:
